b9715e25afa9f0b84687d6a3cd7ba2fc293f0a37
匙羹 <tangspoon@163.com>
2020-12-16 11:36:27 +0800

    删除邮箱账号密码与target文件

--- target/roothub/resources/js/tag/detail.js
+++
@@ -1,7 +0,0 @@
-/*$(function() {
-	//格式化时间
-	$(".formate-date").each(function(i, e) {
-		// console.log(formatDate(Date.parse($(this).text())));
-		$(this).text(formatDate(Date.parse($(this).text())));
-	})
-});*/
